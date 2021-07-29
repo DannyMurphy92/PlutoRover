02c40b921feb6c84ac32d24cac9b30b63cf1c52d
Danny Murphy <Danny.Murphy@decision.tech>
2021-07-29 19:32:49 +0100

    Added simple console application to interact with rover

--- src/PlutoRover/Program.cs
+++ src/PlutoRover/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PlutoRover.Enums;
+using PlutoRover.Services;
+using System;
+using System.Collections.Generic;
 
 namespace PlutoRover
 {
@@ -6,7 +9,35 @@ namespace PlutoRover
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var obstacles = new List<(int x, int y)>();
+            var obstacleService = new ObstacleService(obstacles);
+
+            var maxX = 100;
+            var maxY = 200;
+
+            var rover = new Rover(0, 0, Heading.N, maxX, maxY, obstacleService);
+
+            Console.WriteLine("After travelling many fractions of a lightyear you have finally made it to Pluto...");
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("You can move using the command F: go forward, B: go backward, L: turn left, R: turn right");
+            Console.WriteLine("You can chain commands together such as \"FRFFBLR\"");
+            Console.WriteLine("You can also leave this micro-planet by passing the command \"exit\" at any time");
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Now that thats out of the way, where do you want to go on this barren wasteland?");
+            Console.WriteLine(Environment.NewLine);
+
+            var input = Console.ReadLine();
+
+
+            while(!string.Equals(input, "exit", StringComparison.CurrentCultureIgnoreCase))
+            {
+                rover.Command(input);
+                Console.WriteLine($"Your current position is now: {rover.Position}");
+                Console.WriteLine("What would you like to do next?");
+                input = Console.ReadLine();
+            }
         }
     }
 }
